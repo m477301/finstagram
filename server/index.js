6d79480bc25c5ecfbc8fde66ff434ad6961b438c
Matteo McGuinness <matteo.mcguinness@gmail.com>
2024-07-15 12:49:02 +0200

    7

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express()
 
+const db = require('./models');
+
 require('dotenv').config()
 
 app.use(express.json())
@@ -8,6 +10,8 @@ app.use(express.json())
 const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
 
-app.listen(5555, () => {
-    console.log("Server running on PORT 5555")
+db.sequelize.sync().then(() => {
+    app.listen(5555, () => {
+        console.log("Server running on PORT 5555")
+    })
 })
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router()
 
+const { users } = require("../models")
+
 router.get('/', async (req, res) => {
     return res.send(false);
+})
+
+router.post('/', async (request, res) => {
+
+    const {email, password} = request.body;
+
+    try {
+        await users.create({
+            email: email,
+            password: password
+        });
+    } catch(e) {
+        return res.json({ error: e});
+    }
+
+    return res.json({message: "User has been CREATED"});
+
 })
 
 module.exports = router;
